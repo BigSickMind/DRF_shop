54aef2c7d886dd1b0d27cb9299036ec8602562bb
Egor Smirnov <ghostmen465@gmail.com>
2019-10-07 20:44:10 +0400

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('users/', UserListAPIView.as_view()),
+    path('users/<int:pk>/', UserAPIView.as_view()),
+
+    path('category/', ProductCategoryListAPIView.as_view()),
+    path('category/<int:pk>/', ProductCategoryAPIView.as_view()),
+
+    path('product/', ProductListAPIView.as_view()),
+    path('product/<int:pk>/', ProductAPIView.as_view()),
+
+    path('order/create/', OrderCreateAPIView.as_view()),
+    path('order/', OrderListAPIView.as_view()),
+    path('order/<int:pk>/cancel/', OrderCancelAPIView.as_view()),
+]
